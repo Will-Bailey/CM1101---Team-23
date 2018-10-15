6d7c36b450453fa452f69aef1f7b85520f449969
William Bailey <will.e.bailey@gmail.com>
2018-10-15 16:52:46 +0100

    Here's the dictionary of suspects

---
+++ suspects.py
@@ -0,0 +1,74 @@
+suspect_example = {
+	"name": "", #A name (more friendly than person_example) to refer to this person by in the game 
+	"hair colour": "", #The hair colour of the suspect
+	"sex": "", #The suspect's gender
+	"build": "", #The body type if the suspect
+}
+
+suspect_hollie = {
+	"name": "Hollie",
+	"hair colour": "black",
+	"sex": "female",
+	"build": "tall",
+}
+
+suspect_alexa = {
+	"name": "Alexa",
+	"hair colour": "blonde",
+	"sex": "female",
+	"build": "tall",
+}
+
+suspect_samantha = {
+	"name": "Samantha",
+	"hair colour": "black",
+	"sex": "female",
+	"build": "short",
+}
+
+suspect_wilma = {
+	"name": "Wilma",
+	"hair colour": "blonde",
+	"sex": "female",
+	"build": "short",
+}
+
+suspect_douglas = {
+	"name": "Douglas",
+	"hair colour": "black",
+	"sex": "male",
+	"build": "tall",
+}
+
+suspect_matt = {
+	"name": "Matt",
+	"hair colour": "blonde",
+	"sex": "male",
+	"build": "short",
+}
+
+suspect_raphael = {
+	"name": "Raphael",
+	"hair colour": "black",
+	"sex": "male",
+	"build": "short",
+}
+
+suspect_connor = {
+	"name": "Connor",
+	"hair colour": "blonde",
+	"sex": "male",
+	"build": "tall",
+}
+
+
+suspects = {
+	"Hollie": suspect_hollie,
+	"Alexa": suspect_alexa,
+	"Samantha": suspect_samantha,
+	"Wilma": suspect_wilma,
+	"Douglas": suspect_douglas,
+	"Matt": suspect_matt,
+	"Raphael": suspect_raphael,
+	"Connor": suspect_connor,
+}
