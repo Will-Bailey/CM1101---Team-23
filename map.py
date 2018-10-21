b50d05c57932fc806959aa2cf33bfa35c6d0cd24
Princess-10 <44162753+Princess-10@users.noreply.github.com>
2018-10-21 14:01:33 +0100

    Add files via upload

--- map.py
+++ map.py
@@ -1,81 +1,34 @@
-from items import *
+ room_study = {
+ "name": "Study",
+ "exits": {"east": "Lobby", "secret": "Kitchen"}
 
-room_lobby = {
-    "name": "Lobby",
+ }
+room_lobby ={
+ "name": "Lobby",
+ "exits": {"north": "Conservatory", "east": "Lounge", "north-east": "Dining Room"}
+ }
+room_conservatory = {
+ "name": "Conservatory",
+ "exits": {"south": "Lobby"}
+ }
+room_diningroom = {
+ "name": "Dining Room",
+ "exits": {"south-west": "Lobby", "norht-east": "Kitchen"}
+ }
+room_lounge = {
+ "name": "Lounge",
+ "exits": {"west": "Lobby", "north" : "Kitchen"}
+ }
+room_kitchen = {
+ "name" : "Kitchen",
+ "exits": {"south-west": "Dining Room", "secret": "Study"}
+ }
 
-    "description":
-    """You are in a maze of twisty little passages, all alike.
-Next to you is the School of Computer Science and
-Informatics reception. The receptionist, Matt Strangis,
-seems to be playing an old school text-based adventure
-game on his computer. There are corridors leading to the
-south and east. The exit is to the west.""",
-
-    "exits": {"south": "Admins", "east": "Tutor", "west": "Parking"},
-
-    "items": [item_biscuits, item_handbook]
-}
-
-room_admins = {
-    "name": "MJ and Simon's room",
-
-    "description":
-    """You are leaning agains the door of the systems managers'
-room. Inside you notice Matt "MJ" John and Simon Jones. They
-ignore you. To the north is the reception.""",
-
-    "exits":  {"north": "Lobby"},
-
-    "items": []
-}
-
-room_tutor = {
-    "name": "your personal tutor's office",
-
-    "description":
-    """You are in your personal tutor's office. He intently
-stares at his huge monitor, ignoring you completely.
-On the desk you notice a cup of coffee and an empty
-pack of biscuits. The reception is to the west.""",
-
-    "exits": {"west": "Reception"},
-
-    "items": []
-}
-
-room_parking = {
-    "name": "the parking lot",
-
-    "description":
-    """You are standing in the Queen's Buildings parking lot.
-You can go south to the COMSC reception, or east to the
-general office.""",
-
-    "exits": {"east": "Office", "south": "Lobby"},
-
-    "items": []
-}
-
-room_office = {
-    "name": "the general office",
-
-    "description":
-    """You are standing next to the cashier's till at
-30-36 Newport Road. The cashier looks at you with hope
-in their eyes. If you go west you can return to the
-Queen's Buildings.""",
-
-    "exits": {"west": "Parking"},
-
-    "items": [item_pen]
-}
-
-
-
-rooms = {
+ rooms = {
     "Lobby": room_lobby,
-    "Admins": room_admins,
-    "Tutor": room_tutor,
-    "Parking": room_parking,
-    "Office": room_office
+    "Study": room_study,
+    "Conservatory": room_conservatory,
+    "Dining Room": room_diningroom,
+    "Lounge": room_lounge,
+    "Kitchen": room_kitchen
 }
