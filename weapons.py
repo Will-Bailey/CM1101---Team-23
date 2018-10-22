8b4c8a6108e265293798e5ca28864a8aad4928cf
William Bailey <will.e.bailey@gmail.com>
2018-10-22 01:21:43 +0100

    Removed some un-necessary files and added in weapons.py

---
+++ weapons.py
@@ -0,0 +1,44 @@
+weapon_knife = {
+	"name": "Knife",
+	"description": "Knife description",
+	"notebook_status": "neutral"
+}
+
+weapon_wrench = {
+	"name": "Wrench",
+	"description": "Wrench description",
+	"notebook_status": "neutral"
+}
+
+weapon_pen = {
+	"name": "Pen",
+	"description": "pen description",
+	"notebook_status": "neutral"
+}
+
+weapon_toothbrush = {
+	"name": "Toothbrush",
+	"description": "toothbrush description",
+	"notebook_status": "neutral"
+}
+
+weapon_bottle = {
+	"name": "Bottle",
+	"description": "bottle description",
+	"notebook_status": "neutral"
+}
+
+weapon_hammer = {
+	"name": "Hammer",
+	"description": "Hammer description",
+	"notebook_status": "neutral"
+}
+
+weapons = {
+	"Knife": weapon_knife,
+	"Wrench": weapon_wrench,
+	"Pen": weapon_pen,
+	"Toothbrush": weapon_toothbrush,
+	"Bottle": weapon_bottle,
+	"Hammer": weapon_hammer
+}
